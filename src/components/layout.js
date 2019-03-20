d3701cd61f39d1b365a8977de22900b161ba60ad
connor <connor.jaksik@gmail.com>
2019-03-20 09:48:25 -0600

    updated landing page

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 import Subscribe from "./subscribe"
-import Header from './header'
+import Header from './Header'
 
 const Layout = ({ children }) => (
   <StaticQuery
