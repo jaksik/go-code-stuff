678d209ffab5bff2a8c8fb35bbefe8da790f85a7
connor <connor.jaksik@gmail.com>
2019-03-21 17:21:25 -0600

    attempting new netlify build

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,7 +8,7 @@ import React from "react"
 import Helmet from 'react-helmet'
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
-import Header from "./header"
+import Header from "./Header"
 import Subscribe from "./form-subscribe"
 import "./layout.css"
 
