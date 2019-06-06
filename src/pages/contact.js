77159c7453996927ad0b168f6089379f7d0f76d5
connor <connor.jaksik@gmail.com>
2019-06-06 17:57:24 -0600

    testing bare form in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -16,18 +16,14 @@ export default () => (
     `}
     render={data => (
       <Layout>
-        <section className="section">
-          <div className="container">
-            <div className="content">
               <form
                 name="test"
                 method="post"
                 data-netlify="true"
                 data-netlify-honeypot="bot-field"
-                style={{ width: `80%`, margin: `50px auto`, borderStyle: `ridge`, borderRadius: `25px`, backgroundColor: data.site.siteMetadata.theme }}
               >
-                <input style={{ display: `none` }} type="email" /><br />
-                <h4 style={{ color: `white` }}>Ideas, thoughts, coffee?</h4>
+                <input type="hidden" name="bot-field" />
+                <input type="hidden" name="form-name" value="contact" />                <h4 style={{ color: `white` }}>Ideas, thoughts, coffee?</h4>
                 <h4 style={{ color: `white` }}>Contact {data.site.siteMetadata.title}</h4>
                 <label for="name">Name: *</label>
                 <input name="name" placeholder="  First and Last Name" type="text" /><br />
@@ -37,9 +33,6 @@ export default () => (
                 <textarea name="message" placeholder="  Anything else we should know before your lesson?" type="textarea" /><br />
                 <button style={{ margin: `15px auto`, borderRadius: `10px` }}>Submit</button>
               </form>
-            </div>
-          </div>
-        </section>
       </Layout>
     )}
   />
